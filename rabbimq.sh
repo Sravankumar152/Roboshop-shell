0c0d116dda588313c8036a37f9a56afeab534c91
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-02-08 03:39:45 +0530

    pushing rabbitmq

---
+++ rabbimq.sh
@@ -0,0 +1,42 @@
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+#MYSQL_Host=mysql.daws88.online
+#Redis_Host=redis.daws88.online
+
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+cp /home/ec2-user/Roboshop-shell/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>> $LOGS_FILE
+VALIDATE $? "adding rabbitmq.repo"
+
+dnf install rabbitmq-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing rabbitmq"
+
+systemctl enable rabbitmq-server &>> $LOGS_FILE
+systemctl start rabbitmq-server &>> $LOGS_FILE
+VALIDATE $? "startign mongodb"
+
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGS_FILE
+VALIDATE $? "creating user"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGS_FILE
+VALIDATE $? "Setting user"
+
+
+
+
